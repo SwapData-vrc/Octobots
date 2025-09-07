273e0592d696f423bbfbf3058d3a05bfd9cd350d
SwapData-vrc <shivajdraman@gmail.com>
2025-09-06 22:16:05 -0400

    AOE Scrimmage Edition

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,7 @@ void initialize() {
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.autons_add({
     {"Auton", Code},
+    {"AutonR", CodeR},
       {"Drive\n\nDrive forward and come back", drive_example},
       {"Turn\n\nTurn 3 times.", turn_example},
       {"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
